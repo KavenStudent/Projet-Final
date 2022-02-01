10eb904bd4d8c7c5bb3ebdde189e0598a866682b
Vim Hoang Nguyen <vim123@hotmail.fr>
2022-02-01 16:36:37 -0500

    nav-bar-buttons

--- Projet-Final/serveur/pages/header.php
+++ Projet-Final/serveur/pages/header.php
@@ -1,15 +1,17 @@
-<nav class="navbar navbar-expand-lg navbar-custom test">
-  <div class="container-fluid ">
+<nav class="navbar navbar-expand-lg navbar-light navbar-custom">
+  <div class="container-fluid">
     <a class="navbar-brand text-color" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+      <span class="navbar-toggler-icon"><img id='menu-icon' src="Projet-Final/client/public/images/menu-icon.png" alt='menu-icon'></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
         <a class="nav-link active text-color" aria-current="page" href="#">Home</a>
         <a class="nav-link text-color" href="#">Features</a>
-        <a class="nav-link text-color" href="#">Pricing</a>
-        <a class="nav-link disabled text-color">Disabled</a>
+        <div id='divAuthentification'>
+          <button type="button" class="btn btn-primary">Connexion</button>
+          <button type="button" class="btn btn-primary">Inscription</button>
+        </div>
       </div>
     </div>
   </div>
