9c361603f9030bf443a575caee4ef2fb57bc74cf
Vim Hoang Nguyen <vim123@hotmail.fr>
2022-02-01 15:32:43 -0500

    connection au header

--- index.php
+++ index.php
@@ -5,10 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <?php
+        require_once('Projet-Final/serveur/pages/references.html');
+    ?>
 </head>
 
 <body>
-<?php
+    <?php
         require_once('Projet-Final/serveur/pages/header.php');
     ?>
     <h1>Test 4</h1>
