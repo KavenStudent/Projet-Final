f05828b186f00ea2a09126fd72ca50918daa3d73
Vim Hoang Nguyen <vim123@hotmail.fr>
2022-02-01 23:31:03 -0500

    Modification page accueil
    -NavBar responsibe, css fonctionnel pour tout format
    -ajout du titre du site
    -ajout de la barre de recherche

--- Projet-Final/serveur/pages/header.php
+++ Projet-Final/serveur/pages/header.php
@@ -1,20 +1,26 @@
 <nav class="navbar navbar-expand-lg navbar-light navbar-custom">
   <div class="container-fluid">
-    <a class="navbar-brand text-color" href="#">Navbar</a>
+    <a class="navbar-brand text-color" href="index.php">Logo</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"><img id='menu-icon' src="Projet-Final/client/public/images/menu-icon.png" alt='menu-icon'></span>
+      <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav ml-auto">
-        <button type="button"  class="btn btn-light buttonNav">Mon Profil</button>
-        
+    <div class="collapse navbar-collapse itemsMobileVersion" id="navbarNavAltMarkup">
+    <button type="button"  class="btn btn-light buttonNav">Mon Profil</button>
+      
         <div id='divAuthentification'>
           <button type="button"  class="btn btn-light buttonNav" data-bs-toggle="modal" data-bs-target="#modalConnexion">Connexion</button>
           <button type="button" class="btn btn-light buttonNav" data-bs-toggle="modal" data-bs-target="#modalInscription">Inscription</button>
         </div>
-      </div>
+      
     </div>
   </div>
 </nav>
+
+
+<header class="titreDuSite"><span class="premiereLettre">F</span>oruban</header>
+
+<form id='searchBarForm'>
+  <input id='searchBar' type='text' placeholder="Search...">
+</form>
   
   
