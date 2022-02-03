d33d92e73d654769de61d2cb15177c296ef983ba
Vim Hoang Nguyen <vim123@hotmail.fr>
2022-02-03 08:00:15 -0500

    logo
    Ajout du logo

--- Projet-Final/serveur/pages/header.php
+++ Projet-Final/serveur/pages/header.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light navbar-custom">
   <div class="container-fluid">
-    <a class="navbar-brand text-color" href="index.php">Logo</a>
+    <a class="navbar-brand text-color" href="index.php"><img src='Projet-Final/client/public/images/logoFinale.png'>Logo</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
