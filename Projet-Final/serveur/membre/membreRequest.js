84ac991ec034d74a4dd15c53b2c81008d777e992
Scoutlast1132 <robitaillesacha@gmail.com>
2022-02-03 08:23:42 -0500

    on click enreg

--- Projet-Final/serveur/membre/membreRequest.js
+++ Projet-Final/serveur/membre/membreRequest.js
@@ -1,81 +1,67 @@
-function enregistrerMembre(){
-	var form = new FormData(document.getElementById('form-enregistrer-membre'));
+function enregistrerMembre() {
+  var form = new FormData(document.getElementById("form-enregistrer-membre"));
 
-	$.ajax({
-		type : 'POST',
-		url : 'Projet-Final/Membres/membresControleur.php',
-		data : form,
-		dataType : 'json',
-		contentType : false,
-		processData : false,
-		success : function (reponse){
-			if(reponse.msg != null){
-				initialiser(reponse.msg); // msg = Email deja utilise
-				$("#modal-Membre").modal('hide');
-
-			}else if (reponse.idMembre != null){
-				window.location.reload();
-			}
-		},
-		fail : function (err){
-		 
-		}
-	});
+  $.ajax({
+    type: "POST",
+    url: "./Projet-Final/serveur/Membres/membresControleur.php",
+    data: form,
+    dataType: "json",
+    contentType: false,
+    processData: false,
+    success: function (reponse) {
+      if (reponse.msg != null) {
+        initialiser(reponse.msg); // msg = Email deja utilise
+        $("#modal-Membre").modal("hide");
+      } else if (reponse.idMembre != null) {
+        window.location.reload();
+      }
+    },
+    fail: function (err) {},
+  });
 }
 
-function connexion(){
-	var form = new FormData(document.getElementById('form-connexion'));
+function connexion() {
+  var form = new FormData(document.getElementById("form-connexion"));
 
-	if (!document.getElementById('form-connexion').checkValidity()){
-		document.getElementById('validation-connexion').click();
-
-	}else {
-	
-		$.ajax({
-			type : 'POST',
-			url : 'Projet-Final/Membres/membresControleur.php',
-			data : form,
-			dataType : 'json',
-			contentType : false,
-			processData : false,
-			success : function (reponse){
-				if(reponse.msg != ""){
-					initialiser(reponse.msg); // msg = erreur information connexion 
-					$("#modal-Connexion").modal('hide');
-					document.getElementById('form-connexion').reset();
-				}else {
-					window.location.reload();
-				}
-				
-			},
-			fail : function (err){
-			 
-			}
-		});
-
-	}
-
-	
+  if (!document.getElementById("form-connexion").checkValidity()) {
+    document.getElementById("validation-connexion").click();
+  } else {
+    $.ajax({
+      type: "POST",
+      url: "./Projet-Final/Membres/membresControleur.php",
+      data: form,
+      dataType: "json",
+      contentType: false,
+      processData: false,
+      success: function (reponse) {
+        if (reponse.msg != "") {
+          initialiser(reponse.msg); // msg = erreur information connexion
+          $("#modal-Connexion").modal("hide");
+          document.getElementById("form-connexion").reset();
+        } else {
+          window.location.reload();
+        }
+      },
+      fail: function (err) {},
+    });
+  }
 }
 
-function deconnexion(){
-	var form = new FormData();
-	form.append('action', 'deconnexion');
+function deconnexion() {
+  var form = new FormData();
+  form.append("action", "deconnexion");
 
-	$.ajax({
-		type : 'POST',
-		url : 'Projet-Final/Membres/membresControleur.php',
-		data : form,
-		dataType : 'json',
-		contentType : false,
-		processData : false,
-		success : function (reponse){
-			window.location.reload();
-				membresVue(reponse);
-		},
-		fail : function (err){
-		 
-		}
-	});
-
+  $.ajax({
+    type: "POST",
+    url: "Projet-Final/Membres/membresControleur.php",
+    data: form,
+    dataType: "json",
+    contentType: false,
+    processData: false,
+    success: function (reponse) {
+      window.location.reload();
+      membresVue(reponse);
+    },
+    fail: function (err) {},
+  });
 }
