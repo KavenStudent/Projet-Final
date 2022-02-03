667be0b40448489679c2bf2779d6b2c529c1f50a
joanieBirtz2000 <56734772+joanieBirtz2000@users.noreply.github.com>
2022-02-03 10:01:33 -0500

    page-index
    page-index

--- Projet-Final/serveur/pages/frontPage.php
+++ Projet-Final/serveur/pages/frontPage.php
@@ -2,20 +2,41 @@
 
     <div id='premiereSection'>
         <div id='premiereColonne'>
-            <img id='image-recherche' class ='image-explication' src="Projet-Final/client/public/images/default-Image.png">
+            <img id='image-recherche' class='image-explication' src="Projet-Final/client/public/images/default-Image.png">
             <div id='explication-portfolio' class='text-Format'>
                 <p>Visionner un Portfolio</p>
             </div>
-            <img id='image-mobile' class ='image-explication' src="Projet-Final/client/public/images/default-Image.png">
+            <img id='image-mobile' class='image-explication' src="Projet-Final/client/public/images/default-Image.png">
         </div>
         <div id='deuxiemeColonne'>
-            <div id='explication-recherche' class='text-Format'> 
+            <div id='explication-recherche' class='text-Format'>
                 <p>Faire une recherche</p>
             </div>
-            <img id='image-visionner' class ='image-explication' src="Projet-Final/client/public/images/default-Image.png">
+            <img id='image-visionner' class='image-explication' src="Projet-Final/client/public/images/default-Image.png">
             <div id='explication-mobile' class='text-Format'>
                 <p>Utiliser mobile</p>
             </div>
+        </div>
+    </div>
+
+    <div id='deuxiemeSection'>
+        <div id='type-abonnement'>
+            <div id='abonnement-free' class='case-abonnement'>
+                <p>FREE</p>
+                <p>blabla</p>
+                <button id='sign-button'>Sign Up</button>
+            </div>
+            <div id='abonnement-premium' class='case-abonnement'>
+                <p>PREMIUM</p>
+                <p>blabla</p>
+                <button id='upgrade-button'>Upgrade</button>
+            </div>
+        </div>
+        <div id='raison'>
+            <p>Raison</p>
+        </div>
+        <div id='about-us'>
+            <p>Qui nous sommes</p>
         </div>
     </div>
 </div>
