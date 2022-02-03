ff020d50267ef32507c7af1062a4eed576692d91
Scoutlast1132 <robitaillesacha@gmail.com>
2022-02-03 08:07:57 -0500

    Require front page

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -13,11 +14,13 @@
 <body>
     <?php
         require_once('Projet-Final/serveur/pages/header.php');
+        require_once('Projet-Final/serveur/pages/frontPage.php')
     ?>
-    
+
 
     <?php
         require_once('Projet-Final/serveur/pages/modals.php');
     ?>
 </body>
+
 </html>
