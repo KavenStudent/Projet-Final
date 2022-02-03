57ccd9e20aa0832e1d4b9d6844859b784da4fa57
joanieBirtz2000 <56734772+joanieBirtz2000@users.noreply.github.com>
2022-02-03 08:59:02 -0500

    front-page
    front-page et css

--- Projet-Final/serveur/pages/frontPage.php
+++ Projet-Final/serveur/pages/frontPage.php
@@ -1,4 +1,21 @@
 <div id='containerFrontPage'>
 
-
+    <div id='premiereSection'>
+        <div id='premiereColonne'>
+            <img id='image-recherche' class ='image-explication' src="Projet-Final/client/public/images/default-Image.png">
+            <div id='explication-portfolio' class='text-Format'>
+                <p>Visionner un Portfolio</p>
+            </div>
+            <img id='image-mobile' class ='image-explication' src="Projet-Final/client/public/images/default-Image.png">
+        </div>
+        <div id='deuxiemeColonne'>
+            <div id='explication-recherche' class='text-Format'> 
+                <p>Faire une recherche</p>
+            </div>
+            <img id='image-visionner' class ='image-explication' src="Projet-Final/client/public/images/default-Image.png">
+            <div id='explication-mobile' class='text-Format'>
+                <p>Utiliser mobile</p>
+            </div>
+        </div>
+    </div>
 </div>
