aef1290dbb086013633d6ad95a447ad9fbeb5d83
Vim Hoang Nguyen <vim123@hotmail.fr>
2022-02-06 14:00:19 -0500

    font-page refait au complet

--- Projet-Final/serveur/pages/frontPage.php
+++ Projet-Final/serveur/pages/frontPage.php
@@ -1,42 +1,94 @@
-<div id='containerFrontPage'>
 
-    <div id='premiereSection'>
-        <div id='premiereColonne'>
-            <img id='image-recherche' class='image-explication' src="Projet-Final/client/public/images/default-Image.png">
-            <div id='explication-portfolio' class='text-Format'>
-                <p>Visionner un Portfolio</p>
-            </div>
-            <img id='image-mobile' class='image-explication' src="Projet-Final/client/public/images/default-Image.png">
-        </div>
-        <div id='deuxiemeColonne'>
-            <div id='explication-recherche' class='text-Format'>
-                <p>Faire une recherche</p>
-            </div>
-            <img id='image-visionner' class='image-explication' src="Projet-Final/client/public/images/default-Image.png">
-            <div id='explication-mobile' class='text-Format'>
-                <p>Utiliser mobile</p>
-            </div>
-        </div>
+  <div class="container marketing customized-front-page-container">
+    <!-- Three columns of text below the carousel -->
+    <div class="row d-flex customized-row">
+      <div class="col-lg-4 customized-item">
+        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+        <h2>Heading</h2>
+        <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
+        <p><a class="btn btn-secondary" href="#">View details »</a></p>
+      </div><!-- /.col-lg-4 -->
+
+      <div class="col-lg-4 customized-item">
+        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+        <h2>Heading</h2>
+        <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
+        <p><a class="btn btn-secondary" href="#">View details »</a></p>
+      </div><!-- /.col-lg-4 -->
+
+      <div class="col-lg-4 customized-item">
+        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+        <h2>Heading</h2>
+        <p>And lastly this, the third column of representative placeholder content.</p>
+        <p><a class="btn btn-secondary" href="#">View details »</a></p>
+      </div><!-- /.col-lg-4 -->
+
+    </div><!-- /.row -->
+
+
+    <!-- START THE FEATURETTES -->
+
+    <hr class="featurette-divider">
+
+    <div class="row featurette">
+      <div class="col-md-7">
+        <h2 class="featurette-heading">Comment fonctionne la recherche?</h2>
+        <p class="lead">Tapez le nom de la personne ou bien vous pouvez aussi rechercher par tag.</p>
+      </div>
+      <div class="col-md-5">
+        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"></rect><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+
+      </div>
     </div>
 
-    <div id='deuxiemeSection'>
-        <div id='type-abonnement'>
+    <hr class="featurette-divider">
+
+    <div class="row featurette">
+      <div class="col-md-7 order-md-2">
+        <h2 class="featurette-heading">Comment avoir accès au portfolio?</h2>
+        <p class="lead">Cliquer sur le profil de la personne et vous allez avoir accès à ses informations de profil incluant son portfolio et tous ses projets.</p>
+      </div>
+      <div class="col-md-5 order-md-1">
+        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"></rect><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+
+      </div>
+    </div>
+
+    <hr class="featurette-divider">
+
+    <div class="row featurette">
+      <div class="col-md-7">
+         <h2 class="featurette-heading">Version mobile<!-- <span class="text-muted">Checkmate.</span>--></h2> 
+        <p class="lead">Ce site est dynamique entre autre vous pouvez le consulté avec votre appareil mobile!</p>
+      </div>
+      <div class="col-md-5">
+        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"></rect><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+
+      </div>
+    </div>
+
+    <hr class="featurette-divider">
+    <div id='type-abonnement'>
             <div id='abonnement-free' class='case-abonnement'>
-                <p>FREE</p>
-                <p>blabla</p>
+                <p>GRATUIT</p>
+                <ul>
+                    <li>Vous avez un profil</li>
+                    <li>Les informations de contacts incluses</li>
+                    <li>Un portfolio "LIMITÉ"</li>
+                </ul>
                 <button id='sign-button'>Sign Up</button>
             </div>
             <div id='abonnement-premium' class='case-abonnement'>
-                <p>PREMIUM</p>
-                <p>blabla</p>
+                <p>ABONNEMENT</p>
+                <ul>
+                    <li>Vous avez un profil</li>
+                    <li>Les informations de contacts incluses</li>
+                    <li>Un portfolio "ILLIMITÉ"</li>
+                </ul>
                 <button id='upgrade-button'>Upgrade</button>
             </div>
         </div>
-        <div id='raison'>
-            <p>Raison</p>
-        </div>
-        <div id='about-us'>
-            <p>Qui nous sommes</p>
-        </div>
-    </div>
-</div>
+
+    <!-- /END THE FEATURETTES -->
+  </div>
+
