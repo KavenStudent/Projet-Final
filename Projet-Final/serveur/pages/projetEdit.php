a29b7bca160a7d1a65e292f2492a71e2db8eb04a
Onibaku3 <patrick_al-mondalek3@hotmail.com>
2022-02-07 17:52:52 -0500

    changement a la page projetEdit

--- Projet-Final/serveur/pages/projetEdit.php
+++ Projet-Final/serveur/pages/projetEdit.php
@@ -1,56 +1,48 @@
-<form>
-  <!-- 2 column grid layout with text inputs for the first and last names -->
-  <div class="row mb-4">
-    <div class="col">
-      <div class="form-outline">
-        <input type="text" id="form6Example1" class="form-control" />
-        <label class="form-label" for="form6Example1">First name</label>
-      </div>
-    </div>
-    <div class="col">
-      <div class="form-outline">
-        <input type="text" id="form6Example2" class="form-control" />
-        <label class="form-label" for="form6Example2">Last name</label>
-      </div>
-    </div>
-  </div>
+<form class="editProj">
 
-  <!-- Text input -->
-  <div class="form-outline mb-4">
-    <input type="text" id="form6Example3" class="form-control" />
-    <label class="form-label" for="form6Example3">Company name</label>
-  </div>
-
-  <!-- Text input -->
-  <div class="form-outline mb-4">
-    <input type="text" id="form6Example4" class="form-control" />
-    <label class="form-label" for="form6Example4">Address</label>
-  </div>
-
-  <!-- Email input -->
-  <div class="form-outline mb-4">
-    <input type="email" id="form6Example5" class="form-control" />
-    <label class="form-label" for="form6Example5">Email</label>
-  </div>
-
-  <!-- Number input -->
-  <div class="form-outline mb-4">
-    <input type="number" id="form6Example6" class="form-control" />
-    <label class="form-label" for="form6Example6">Phone</label>
+   <!-- Text input -->
+  <div class="form-outline mb-69">
+    <label class="form-label ftxt" for="form6Example3">Titre:</label>
+    <input type="text" id="form6Example3" class="form-control" placeholder="Example" />
+    
   </div>
 
   <!-- Message input -->
-  <div class="form-outline mb-4">
-    <textarea class="form-control" id="form6Example7" rows="4"></textarea>
-    <label class="form-label" for="form6Example7">Additional information</label>
+  <div class="form-outline mb-69">
+    <label class="form-label" for="form6Example7">Description:</label>
+    <textarea class="form-control ftxt" id="form6Example7" rows="4" placeholder="Description..."></textarea>
+    
   </div>
 
-  <!-- Checkbox -->
-  <div class="form-check d-flex justify-content-center mb-4">
-    <input class="form-check-input me-2" type="checkbox" value="" id="form6Example8" checked />
-    <label class="form-check-label" for="form6Example8"> Create an account? </label>
+  <!-- Message input -->
+  <div class="form-outline mb-69">
+    <label class="form-label" for="form6Example7">Participants:</label>
+    <textarea class="form-control ftxt" id="form6Example7" rows="4" placeholder="Patricipants..."></textarea>
+    
   </div>
 
+  <!-- Email input -->
+  <div class="form-outline mb-69">
+    <label class="form-label" for="form6Example5">Lien:</label>
+    <input type="url" id="form6Example5" class="form-control" placeholder="Lien..." />
+    
+  </div>
+
+  <div class="col-md-5 order-md-1 customize">
+
+  <img id="output" src="Projet-Final/client/public/images/default-image.png" class="rounded mx-auto d-block" height="600px" width="600px">
+  
+
+  <div class="form-outline-inpt inpt">
+  <input class="form-control" type="file" accept="image/*" onchange="loadFile(event)">
+  </div>
+
+    <div class="form-check form-switch">
+      <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked />
+      <label class="form-check-label" for="flexSwitchCheckChecked">Visibilite</label>
+      </div>
+</div>
   <!-- Submit button -->
-  <button type="submit" class="btn btn-primary btn-block mb-4">Place order</button>
+  <button type="submit" class="btn btn-primary btn-block mb-4 canBtn">Cancel</button>
+  <button type="submit" class="btn btn-primary btn-block mb-4">Sauvegarder</button>
 </form>
