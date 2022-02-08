ded57202b2f98718fc565e5b9f3a01f5b0fb72e2
Onibaku3 <patrick_al-mondalek3@hotmail.com>
2022-02-08 15:38:48 -0500

    Update globalScript.js

--- Projet-Final/client/public/js/globalScript.js
+++ Projet-Final/client/public/js/globalScript.js
@@ -60,10 +60,3 @@ function closeToast() {
   $("#toastForm").toast("hide");
 }
 
-var loadFile = function(event) {
-  var output = document.getElementById('output');
-  output.src = URL.createObjectURL(event.target.files[0]);
-  output.onload = function() {
-    URL.revokeObjectURL(output.src) // free memory
-  }
-};
