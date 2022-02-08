7e395cda5f186176d02694a00f7bae3efbdfdbd3
Scoutlast1132 <robitaillesacha@gmail.com>
2022-02-08 17:54:02 -0500

    Commencer projetDAOImpl

--- Projet-Final/serveur/projet/projetDAO.php
+++ Projet-Final/serveur/projet/projetDAO.php
@@ -6,6 +6,6 @@ interface ProjetDao
     public function getProjet(int $id): Projet;
 
     //Retourne tous les projets
-    public function getAllProjet(): array;
+    public function getAllProjetsForMembre(): array;
 }
 ?>
--- Projet-Final/serveur/projet/projetDAOImpl.php
+++ Projet-Final/serveur/projet/projetDAOImpl.php
@@ -1,8 +1,25 @@
 <?php
 class MembreDaoImpl extends Modele implements MembreDao
 {
-    public function getProjet(): Projet {
+    public function getProjet($id): Projet {
         
+    }
+    public function getAllProjetsForMembre($idMembre): array {
+        try {
+            $tab = array();
+            $requete = "SELECT id, titre, description, thumbnail FROM projet WHERE idCreateur = ?";
+            $this->setRequete($requete);
+            $this->setParams(array($idMembre));
+            $stmt = $this->executer();
+            while ($ligne = $stmt->fetch(PDO::FETCH_OBJ)) {
+                $tab[] = $ligne;
+            }
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        } finally {
+            unset($requete);
+        }
+        return $tab;
     }
 }
 ?>
