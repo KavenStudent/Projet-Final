3961488e5b02df40d4dfb4a61e195a97ac1370a5
Scoutlast1132 <robitaillesacha@gmail.com>
2022-02-10 11:18:12 -0500

    Avancement projetDAO

--- Projet-Final/serveur/projet/projetDAO.php
+++ Projet-Final/serveur/projet/projetDAO.php
@@ -1,11 +1,32 @@
 <?php 
+require_once("projet.php");
+require_once("projetDAO.php");
+require_once("../includes/modele.inc.php");
 //Interface MembreDao
 interface ProjetDao
 {
     //Retourne un projet
-    public function getProjet(int $id): Projet;
+    public function getProjet(int $idProjet): Projet;
 
     //Retourne tous les projets
-    public function getAllProjetsForMembre(): array;
+    public function getAllProjets(): array;
+
+    //Retourne tous les projets pour un certain membre
+    public function getAllProjetsForMembre(int $idMembre): array;
+
+    //Retourne tous les participants d'un projet
+    public function getAllRegisteredParticipantsForProjet(int $idProjet): array;
+
+    //Retourne tous les tags d'un projet
+    public function getAllTagsForProjet(int $idProjet): array;
+
+    //Cree un nouveau projet
+    public function creerProjet(Projet $projet, array $tags, array $participants);
+
+    //Modifie un projet donné
+    public function modifierProjet(Projet $projet);
+
+    //Télécharge le fichier d'un projet
+    public function telechargerProjet(int $idProjet);
 }
 ?>
