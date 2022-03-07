1d959249e2c9c095d55904c1591a9c7eb47942bb
Vim Hoang Nguyen <vim123@hotmail.fr>
2022-03-07 14:41:49 -0500

    ajout requete

---
+++ Projet-Final/serveur/paypal/paypalController.php
@@ -0,0 +1,31 @@
+<?php
+//Controller
+$tabRes = array();
+$tabRes['action'] = null;
+$action = $_POST['action'];
+switch ($action) {
+    case "payer":
+        payer();
+        break;
+}
+
+
+function payer(){
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ Projet-Final/serveur/paypal/paypalRequete.js
@@ -0,0 +1,41 @@
+const PRIX_ABONNEMENT = 4.99;
+
+function afficherPaypal(){
+    
+
+    item = {
+      prix: PRIX_ABONNEMENT,
+      abonnement_creation: Date.now(),
+    }
+
+    paypal.Buttons({
+      createOrder: function (data, actions) {
+          // This function sets up the details of the transaction, including the amount and line item details.
+          return actions.order.create({
+              purchase_units: [{
+                  amount: {
+                      value: PRIX_ABONNEMENT
+                  }
+              }]
+          });
+      },
+      onApprove: function () {
+
+          $.ajax({
+              type: 'POST',
+              url: './Projet-Final/serveur/paypal/paypalController.php',
+              data: {"action": 'payer', "item": item},
+              dataType: 'json',
+              success: function (reponse) {
+                  // viderPanier();
+                  // initialiser(reponse.msg);
+              },
+              fail: function (err) {}
+          });
+
+      }
+  }).render('#paypal-button-container');
+    
+    
+  
+  }
