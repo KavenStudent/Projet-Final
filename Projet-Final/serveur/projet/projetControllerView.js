9a98afd2b8ccef88da062d20d46018c6b819f675
KavenStudent <201490096@collegeahuntsic.qc.ca>
2022-03-21 14:56:15 -0400

    Update projetControllerView.js

--- Projet-Final/serveur/projet/projetControllerView.js
+++ Projet-Final/serveur/projet/projetControllerView.js
@@ -163,7 +163,7 @@ function afficherPageProjetEdit(json) {
     });
   }
 
-  contenu += `<input id="participantsInput" type="text" onkeypress="return /[0-9a-zA-Z -]/i.test(event.key)" />
+  contenu += `<input id="participantsInput" type="text" onkeypress="return /[a-zA-Z -]/i.test(event.key)" />
   </div>
   <p class="noteEnter">Note: Utiliser "enter" pour ajouter un participant.</p>
 
@@ -310,7 +310,7 @@ function ajouterProjetAffichage(json) {
      <label class="form-label" for="participantsProjet">Participants:</label>
       <!-- PARTICIPANTS TAGS -->
      <div class="participant-container">
-        <input id="participantsInput" type="text" onkeypress="return /[0-9a-zA-Z -]/i.test(event.key)" />
+        <input id="participantsInput" type="text" onkeypress="return /[a-zA-Z -]/i.test(event.key)" />
       </div>
       <p class="noteEnter">Note: Utiliser "enter" pour ajouter un participant.</p>
 
